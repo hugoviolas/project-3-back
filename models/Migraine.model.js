c6a22f7e77a63b84133cd2b6b91f578d2d23a498
Hugo Violas <violas.hugo@gmail.com>
2022-11-30 17:02:24 +0100

    Add migraine route with migraine creation request, modify routes names

--- models/Migraine.model.js
+++ models/Migraine.model.js
@@ -1,49 +1,55 @@
 const { Schema, model } = require("mongoose");
 
 const migraineSchema = new Schema(
-    {
-        start_date: {
-            type: Date,
-            required: true,
-            default: Date.now,
-        },
-        end_date: {
-            type: Date,
-        },
-        intensity: {
-            type: Number,
-            min: 0,
-            max: 10
-        },
-        phases: [{
-            type: String,
-            enum: ["Prodrome", "Aura", "Headache", "Postdrome", "Other/Unsure"]
-        }],
-        user: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-        },
-        selected_trackers: [{
-            type: Schema.types.ObjectId,
-            ref: "Tracker"
-        }],
-        selected_treatments: [{
-            type: Schema.types.ObjectId,
-            ref: "Treatment"
-        }],
-        main_trigger: {
-            type: Schema.types.ObjectId,
-            ref: "Tracker"
-        },
-        notes: {
-            type: String,
-            maxLength: 1000
-        }
+  {
+    start_date: {
+      type: Date,
+      required: true,
+      default: Date.now,
     },
-    {
-        // `createdAt` and `updatedAt`
-        timestamps: true,
-    }
+    end_date: {
+      type: Date,
+    },
+    intensity: {
+      type: Number,
+      min: 0,
+      max: 10,
+    },
+    phases: [
+      {
+        type: String,
+        enum: ["Prodrome", "Aura", "Headache", "Postdrome", "Other/Unsure"],
+      },
+    ],
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    selected_trackers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Tracker",
+      },
+    ],
+    selected_treatments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Treatment",
+      },
+    ],
+    main_trigger: {
+      type: Schema.Types.ObjectId,
+      ref: "Tracker",
+    },
+    notes: {
+      type: String,
+      maxLength: 1000,
+    },
+  },
+  {
+    // `createdAt` and `updatedAt`
+    timestamps: true,
+  }
 );
-
-module.exports = model("Migraine", migraineSchema);
+const Migraine = model("Migraine", migraineSchema);
+module.exports = Migraine;
---
+++ routes/migraines.routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const protectRoute = require("../middlewares/protectRoute");
+const Migraine = require("../models/Migraine.model");
+const Tracker = require("../models/Tracker.model");
+
+router.get("/", (req, res, next) => {
+  res.send("Migraines route");
+});
+
+router.post("/", async (req, res, next) => {
+  console.log(req.body);
+  const newMigraine = await Migraine.create(req.body);
+  res.status(201).json(newMigraine);
+});
+
+module.exports = router;
