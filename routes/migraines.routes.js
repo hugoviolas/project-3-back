f4c8cb35846c0eb856c27db9b74387e1d4180222
Ines Zallouz <ines.zallouz@gmail.com>
2022-12-02 11:35:33 +0100

    Create delete migraine route

--- routes/migraines.routes.js
+++ routes/migraines.routes.js
@@ -4,12 +4,14 @@ const protectRoute = require("../middlewares/protectRoute");
 const Migraine = require("../models/Migraine.model");
 const Tracker = require("../models/Tracker.model");
 
+// Get all migraines for a user
 router.get("/", isAuthenticated, async (req, res, next) => {
   const userID = req.user.id;
   const allMigraines = await Migraine.find({ user: userID });
   res.status(200).json(allMigraines);
 });
 
+// Create a new migraine
 router.post("/", isAuthenticated, async (req, res, next) => {
   const data = req.body;
   data["user"] = req.user.id;
@@ -17,6 +19,7 @@ router.post("/", isAuthenticated, async (req, res, next) => {
   res.status(201).json(newMigraine);
 });
 
+// See the details of one migraine
 router.get("/:id", isAuthenticated, async (req, res, next) => {
   try {
     const oneMigraine = await Migraine.findById(req.params.id)
@@ -26,5 +29,17 @@ router.get("/:id", isAuthenticated, async (req, res, next) => {
     next(error)
   }
 })
+
+// Edit a migraine
+
+// Delete a migraine
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Migraine.findByIdAndRemove(req.params.id);
+    res.status(200)
+  } catch (error) {
+    next(err);
+  }
+});
 
 module.exports = router;
